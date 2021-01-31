b253ea5c69d9510b5844f0b4a91c7a204f91c4c9
serpent <66120473+serpent0917@users.noreply.github.com>
2021-02-01 01:37:37 +0900

    Add files via upload

---
+++ lib/gcd_lcm.py
@@ -0,0 +1,15 @@
+# x, yの最大公約数
+def gcd(x, y):
+    if (y == 0):
+        return x
+    else:
+        return gcd(y, x % y)
+
+# x, yの最小公倍数
+def lcm(x, y):
+    return (x * y) // gcd(x, y)
+
+
+a, b = map(int, input().split())
+print(gcd(a, b))
+print(lcm(a, b))
