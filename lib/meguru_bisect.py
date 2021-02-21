24f50125bb50361bfa4966066ef9bfc0aefc7dc8
serpent <66120473+serpent0917@users.noreply.github.com>
2021-02-22 01:10:51 +0900

    Update meguru_bisect.py

--- lib/meguru_bisect.py
+++ lib/meguru_bisect.py
@@ -4,12 +4,10 @@ def is_ok(arg):
     pass
 
 def meguru_bisect(ng, ok):
-    """
-    初期値の ng, ok を受け取り, is_ok を満たす最大(最小)の ok を返す
-    まず is_ok を定義する
-    ng, ok は とり得る最大の値 + 1, とり得る最小の値 - 1
-    最小の ok を求める場合は ng, ok を逆にする
-    """
+    # 初期値の ng, ok を受け取り, is_ok を満たす最大(最小)の ok を返す
+    # まず is_ok を定義する
+    # ng, ok は とり得る最大の値 + 1, とり得る最小の値 - 1
+    # 最小の ok を求める場合は ng, ok を逆にする
     while (abs(ok - ng) > 1):
         mid = (ok + ng) // 2
         if is_ok(mid):
