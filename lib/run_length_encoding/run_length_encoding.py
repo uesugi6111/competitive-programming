b3bc558ff8433d50d56a21bdcb223f891394f1cb
serpent <66120473+serpent0917@users.noreply.github.com>
2021-02-22 12:15:19 +0900

    Add files via upload

---
+++ lib/run_length_encoding/run_length_encoding.py
@@ -0,0 +1,15 @@
+# ランレングス圧縮
+def rle(s):
+    res = ""
+    i = 0
+    while (i < len(s)):
+        startpos = i
+        while (i < len(s) and s[startpos] == s[i]):
+            i += 1
+        res += s[startpos]
+        res += str(i - startpos)
+    return res
+
+# 使用例
+s = input()
+print(rle(s))
