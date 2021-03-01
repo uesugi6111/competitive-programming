75b5306b22dd6e64dfa7881dda4f555dae218492
serpent <66120473+serpent0917@users.noreply.github.com>
2021-03-01 18:08:24 +0900

    Update eratosthenes.py

--- lib/eratosthenes/eratosthenes.py
+++ lib/eratosthenes/eratosthenes.py
@@ -3,7 +3,7 @@ def eratosthenes(n):
     is_prime = [True] * (n + 1)
     is_prime[0] = is_prime[1] = False
     for i in range(2, n + 1):
-        if (is_prime[i]):
+        if is_prime[i]:
             for j in range(i * i, n + 1, i):
                 is_prime[j] = False
     return is_prime
