c9657f39513496eb5a3654d5877ee6f93c553a05
serpent <66120473+serpent0917@users.noreply.github.com>
2021-03-01 18:06:08 +0900

    Update run_length_encoding.py

--- lib/run_length_encoding/run_length_encoding.py
+++ lib/run_length_encoding/run_length_encoding.py
@@ -2,9 +2,9 @@
 def rle(s):
     res = ""
     i = 0
-    while (i < len(s)):
+    while i < len(s):
         startpos = i
-        while (i < len(s) and s[startpos] == s[i]):
+        while i < len(s) and s[startpos] == s[i]:
             i += 1
         res += s[startpos]
         res += str(i - startpos)
