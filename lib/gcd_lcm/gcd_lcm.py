310729abe60fc0567d3367c8133832132bc1f1ae
serpent <66120473+serpent0917@users.noreply.github.com>
2021-03-02 21:57:56 +0900

    Update gcd_lcm.py

--- lib/gcd_lcm/gcd_lcm.py
+++ lib/gcd_lcm/gcd_lcm.py
@@ -3,11 +3,13 @@ def gcd(a, b):
     if a == 0: return b
     else: return gcd(b % a, a)
 
+
 # a, bの最小公倍数
 def lcm(a, b):
     return (a * b) // gcd(a, b)
 
+
 # 使用例
-a, b = map(int, input().split())
-print(gcd(a, b))
-print(lcm(a, b))
+x, y = map(int, input().split())
+print(gcd(x, y))
+print(lcm(x, y))
