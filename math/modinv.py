d9cff57de19c874cc07314d737c78fd8b14257a3
serpent <66120473+serpent0917@users.noreply.github.com>
2021-06-25 04:41:15 +0900

    Add files via upload

---
+++ math/modinv.py
@@ -0,0 +1,17 @@
+# 拡張ユークリッドの互除法
+def ext_gcd(a, b):
+    if b == 0:
+        return (1, 0, a)
+    else:
+        (X, Y, g) = ext_gcd(b, a % b)
+        return (Y, X - a // b * Y, g)
+
+# mod m における a の逆元 (a と m が互いに素であることが必要)
+# ax + my = 1 <=> ax = 1(mod m)
+def modinv(a, m):
+    x, y, g = ext_gcd(a, m)
+    return x % m
+
+# 使用例
+a, m = map(int, input().split())
+print(modinv(a, m))
