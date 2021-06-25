747b6d1ef41ff6565dd40beede49ed533d431f96
serpent <66120473+serpent0917@users.noreply.github.com>
2021-06-25 14:17:07 +0900

    Update modinv.py

--- math/modinv.py
+++ math/modinv.py
@@ -7,11 +7,13 @@ def ext_gcd(a, b):
         return (Y, X - a // b * Y, g)
 
 # mod m における a の逆元 (a と m が互いに素であることが必要)
-# ax + my = 1 <=> ax = 1(mod m)
+# ax + my = 1 <=> ax ≡ 1(mod m)
 def modinv(a, m):
     x, y, g = ext_gcd(a, m)
     return x % m
 
 # 使用例
-a, m = map(int, input().split())
-print(modinv(a, m))
+# a / b % mod = a * b ^ {-1} % mod
+mod = 10 ** 9 + 7
+a, b = map(int, input().split())
+print(a * modinv(b, mod) % mod)
