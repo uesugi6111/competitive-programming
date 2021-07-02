69277ffab57e6b5cd564a28a273df1ade0951259
serpent <66120473+serpent0917@users.noreply.github.com>
2021-07-02 23:06:47 +0900

    Update modpow.py

--- math/combinatorics/modpow.py
+++ math/combinatorics/modpow.py
@@ -1,5 +1,7 @@
 # a ^ n % mod
 def modpow(a, n, mod):
+    if mod == 1:
+        return 0
     res = 1
     while(n > 0):
         if (n & 1): res = res * a % mod  # n の最下位bitが 1 ならば a ^ (2 ^ i) をかける
